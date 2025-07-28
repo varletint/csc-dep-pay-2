213abc68ea624cb16d2c6e596ca4e244ca5ad2fa
varletint <intvarlet@gmail.com>
2025-07-28 18:08:01 +0100

    secs

--- client/src/compponents/CreateItem.jsx
+++ client/src/compponents/CreateItem.jsx
@@ -39,7 +39,7 @@ export default function CreateItem({ close }) {
         setShowModal(true);
         setTimeout(() => {
           setShowModal(false);
-        }, 988);
+        }, 20000);
 
         // setShowModal(false);
         console.log(data.items);
