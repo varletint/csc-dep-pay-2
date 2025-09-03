7c8f7f0e9ecbd4b1e03f23414a85e4a9888486df
varletint <intvarlet@gmail.com>
2025-09-03 08:06:38 +0100

    fix: simplify token retrieval and enhance error handling in user verification

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 import { errorHandler } from "./errorHandler.js";
 
 export const verifyUserToken = (req, res, next) => {
-  const token = req.cookies?.access_token;
+  const token = req.cookies.access_token;
 
   if (!token) {
     // return next(errorHandler(401, "Valid token not found"));
@@ -14,17 +14,19 @@ export const verifyUserToken = (req, res, next) => {
   }
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     // if (err) return next(errorHandler(401, "Unauthorized"));
-    // if (err) {
-    //   if (req.originalUrl.startsWith("/api")) {
-    //     return next(errorHandler(401, "Unauthorized"));
-    //   }
-    //   return res.redirect("/login");
-    // }
-    return res.status(401).render("errorPage", {
-      title: "Unauthorized",
-      message: "Your session has expired or the token is invalid.",
-      redirectUrl: "/login",
-    });
+    if (err) {
+      return res.status(401).render("errorPage", {
+        title: "Unauthorized",
+        message: "Your session has expired or the token is invalid.",
+        redirectUrl: "/login",
+      });
+
+      // if (req.originalUrl.startsWith("/api")) {
+      //   return next(errorHandler(401, "Unauthorized"));
+      // }
+      // return res.redirect("/login");
+    }
+
     req.user = user;
     next();
   });
